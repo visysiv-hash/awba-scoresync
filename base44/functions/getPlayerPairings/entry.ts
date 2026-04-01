a5007dd52fd6a693c3ecfe4dba88de82690eb61f
Visy Rangubhatla <visysiv@gmail.com>
2026-04-01 09:35:56 +0000

    File changes

--- base44/functions/getPlayerPairings/entry.ts
+++ base44/functions/getPlayerPairings/entry.ts
@@ -13,7 +13,7 @@ Deno.serve(async (req) => {
     const sheetId = Deno.env.get('SPREADSHEET_ID');
     
     const gamesUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/raw_response!A:Z`;
-    const gamesRes = await fetch(`${gamesUrl}?key=${Deno.env.get('GOOGLE_API_KEY')}`, {
+    const gamesRes = await fetch(gamesUrl, {
       headers: { 'Authorization': `Bearer ${accessToken}` }
     });
     const gamesData = await gamesRes.json();
